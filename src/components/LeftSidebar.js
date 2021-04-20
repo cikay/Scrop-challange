0ca497d856564725eead90b4ed8be7381d93b557
cıkay <muzaffer123cikay@gmail.com>
2021-04-20 22:20:47 +0300

    added page name to leftsidebar

--- src/components/LeftSidebar.js
+++ src/components/LeftSidebar.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { useLocation } from 'react-router-dom'
-const pagePathToAccesor = {
+const pagePathNameToAccesor = {
   '/': 'home',
-  contactus: 'contactus',
+  '/contactus': 'contactus',
 }
+
 function LeftSidebar() {
   const { t } = useTranslation()
   const location = useLocation()
-  console.log('location', location)
-  const pageNameAccessor = pagePathToAccesor[location.pathname]
+  const letfSidebarObj = t('leftSideBar', { returnObjects: true })
 
+  console.log('location', location)
+  const pageNameAccessor = pagePathNameToAccesor[location.pathname]
+  const pageName = letfSidebarObj[pageNameAccessor]
+  console.log('pageNameAccessor', pageNameAccessor)
+  console.log('page name', pageName)
+  console.log('letfSidebarObj', letfSidebarObj)
   return (
     <div className='p-2'>
-      <h3>{t(pageNameAccessor)}</h3>
+      <h3>{pageName}</h3>
     </div>
   )
 }
